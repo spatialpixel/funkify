849344595d38502493299b4b9b977c6a9ac7c9f6
William Martin <william@spatialpixel.com>
2024-03-31 20:57:11 -0400

    Move js files to src

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import './style.css';
 
-import './dropdown-menu.js';
-import './tools-list.js';
-import './tool-editor.js';
-import './parameter-item.js';
-import './collapsible-element.js';
+import './src/dropdown-menu.js';
+import './src/tools-list.js';
+import './src/tool-editor.js';
+import './src/parameter-item.js';
+import './src/collapsible-element.js';
 
-import { State } from './state.js';
-import { ChatManager } from './chat-manager.js';
-import { KeyManager } from './key-manager.js';
-import { PromptManager } from './prompt-manager.js';
-import examples from './example-functions.js';
+import { State } from './src/state.js';
+import { ChatManager } from './src/chat-manager.js';
+import { KeyManager } from './src/key-manager.js';
+import { PromptManager } from './src/prompt-manager.js';
+import examples from './src/example-functions.js';
 
 if (document.readyState === 'complete') {
   onReady();
