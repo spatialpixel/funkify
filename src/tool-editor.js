67850fbc981fbd8592f59fd64e1a0b6c9b39ac7c
William Martin <william@spatialpixel.com>
2024-04-02 16:48:41 -0400

    Add CodeMirror editor

--- src/tool-editor.js
+++ src/tool-editor.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import { basicSetup, EditorView } from "codemirror";
+import { EditorState, Compartment } from "@codemirror/state";
+import { language } from "@codemirror/language"
+import { javascript } from "@codemirror/lang-javascript"
+
+const languageConf = new Compartment // Some codemirror thing.
 
 class ToolEditor extends HTMLElement {
   constructor() {
     super();
-
-    const template = document.getElementById('tool-editor-template');
+    
+    const template = document.querySelector('#tool-editor-template');
     const templateContent = template.content;
 
     // Create shadow DOM and append template
@@ -18,63 +24,66 @@ class ToolEditor extends HTMLElement {
     closeButton.addEventListener('click', this.close.bind(this));
     
     const saveButton = this.shadowRoot.querySelector('button.tool-save');
-    saveButton.addEventListener('click', event => {
-      const functionNameField = this.shadowRoot.querySelector('.function-name-field');
-      this.tool.name = functionNameField.value;
-      
-      const functionDescriptionField = this.shadowRoot.querySelector('.function-description-field');
-      this.tool.description = functionDescriptionField.value;
-      
-      const functionImplementationField = this.shadowRoot.querySelector('.function-implementation');
-      this.tool.f = functionImplementationField.value;
-      
-      // Loop through all the parameters' properties and gather them up.
-      const properties = {};
-      const required = [];
-      const parameters = this.shadowRoot.querySelectorAll('parameter-item');
-      
-      for (const parameter of parameters) {
-        const parameterNameElt = parameter.shadowRoot.querySelector('.parameter-name');
-        const parameterName = parameterNameElt.value;
-        
-        const parameterDescElt = parameter.shadowRoot.querySelector('.parameter-description');
-        const parameterDesc = parameterDescElt.value;
-        
-        const parameterTypeElt = parameter.shadowRoot.querySelector('.parameter-type');
-        const parameterType = parameterTypeElt.value;
-        
-        properties[parameterName] = {
-          description: parameterDesc,
-          type: parameterType,
-        };
-        
-        const parameterReqElt = parameter.shadowRoot.querySelector('.parameter-required');
-        const parameterReq = parameterReqElt.value;
-        if (parameterReq) {
-          required.push(parameterName);
-        }
-      }
-      
-      this.tool.properties = properties;
-      this.tool.required = required;
-      
-      const toolsList = document.querySelector('tools-list');
-      toolsList.refresh();
-      
-      this.close();
-    });
+    saveButton.addEventListener('click', this.save.bind(this));
     
     const addParameterButton = this.shadowRoot.querySelector('button.add-parameter');
-    addParameterButton.addEventListener('click', event => {
-      const value = {
-        'type': 'string',
-        'description': 'A generic property'
+    addParameterButton.addEventListener('click', this.addParameter.bind(this));
+  }
+  
+  save (event) {
+    const functionNameField = this.shadowRoot.querySelector('.function-name-field');
+    this.tool.name = functionNameField.value;
+    
+    const functionDescriptionField = this.shadowRoot.querySelector('.function-description-field');
+    this.tool.description = functionDescriptionField.value;
+    
+    this.tool.f = this.getEditorContents();
+    
+    // Loop through all the parameters' properties and gather them up.
+    const properties = {};
+    const required = [];
+    const parameters = this.shadowRoot.querySelectorAll('parameter-item');
+    
+    for (const parameter of parameters) {
+      const parameterNameElt = parameter.shadowRoot.querySelector('.parameter-name');
+      const parameterName = parameterNameElt.value;
+      
+      const parameterDescElt = parameter.shadowRoot.querySelector('.parameter-description');
+      const parameterDesc = parameterDescElt.value;
+      
+      const parameterTypeElt = parameter.shadowRoot.querySelector('.parameter-type');
+      const parameterType = parameterTypeElt.value;
+      
+      properties[parameterName] = {
+        description: parameterDesc,
+        type: parameterType,
       };
-      const name = 'new-parameter';
-      const param = this.renderParameterForm(value, name);
-      const parametersList = this.shadowRoot.querySelector('.parameters-list');
-      parametersList.appendChild(param);
-    });
+      
+      const parameterReqElt = parameter.shadowRoot.querySelector('.parameter-required');
+      const parameterReq = parameterReqElt.value;
+      if (parameterReq) {
+        required.push(parameterName);
+      }
+    }
+    
+    this.tool.properties = properties;
+    this.tool.required = required;
+    
+    const toolsList = document.querySelector('tools-list');
+    toolsList.refresh();
+    
+    this.close();
+  }
+  
+  addParameter (event) {
+    const value = {
+      'type': 'string',
+      'description': 'A generic property'
+    };
+    const name = 'new-parameter';
+    const param = this.renderParameterForm(value, name);
+    const parametersList = this.shadowRoot.querySelector('.parameters-list');
+    parametersList.appendChild(param);
   }
   
   close () {
@@ -93,20 +102,21 @@ class ToolEditor extends HTMLElement {
     const parametersList = this.shadowRoot.querySelector('.parameters-list');
     parametersList.innerHTML = '';
     
+    // Clear the editor
+    this.clearEditor();
+    
+    // Remove the reference to the last tool.
     this.tool = null;
   }
 
   editTool (tool, state) {
     this.tool = tool;
     
-    const toolEditorElt = document.querySelector('tool-editor');
+    const toolEditorElt = this.shadowRoot.host;
     toolEditorElt.style.display = 'block';
     
     const functionNameField = this.shadowRoot.querySelector('.function-name-field');
     functionNameField.value = tool.name;
-    
-    const functionDescriptionField = this.shadowRoot.querySelector('.function-description-field');
-    functionDescriptionField.value = tool.description;
     
     const parametersList = this.shadowRoot.querySelector('.parameters-list');
     _.forEach(tool.properties, (value, name) => {
@@ -114,8 +124,60 @@ class ToolEditor extends HTMLElement {
       parametersList.appendChild(param);
     });
     
-    const functionImplementationField = this.shadowRoot.querySelector('.function-implementation');
-    functionImplementationField.value = tool.f;
+    this.populateEditor();
+  }
+  
+  populateEditor () {
+    if (!this.tool) { return; }
+    
+    if (!this.editorView) {
+      this.createEditor();
+    }
+    
+    this.setEditorContents(this.tool.f);
+  }
+  
+  createEditor () {
+    this.editorContainer = this.shadowRoot.querySelector('#function-implementation');
+    
+    this.editorTheme = EditorView.baseTheme({
+      "&": {
+        fontSize: "20px",
+      },
+      "&.cm-focused": {
+        outline: "none",
+      },
+    });
+    
+    this.editorView = new EditorView({
+      doc: "",
+      extensions: [
+        basicSetup,
+        this.editorTheme,
+        languageConf.of(javascript()),
+        EditorView.lineWrapping,
+      ],
+      parent: this.editorContainer
+    });
+  }
+  
+  setEditorContents (text) {
+    this.editorView.setState(EditorState.create({
+      doc: text,
+      extensions: [
+        basicSetup,
+        languageConf.of(javascript()),
+        EditorView.lineWrapping,
+      ]
+    }));
+  }
+  
+  getEditorContents () {
+    return this.editorView.state.doc.toString();
+  }
+  
+  clearEditor () {
+    this.setEditorContents("");
   }
   
   renderParameterForm (value, name, required) {
