044be7327ec3a6ab97fe3537c49a44e353997b99
William Martin <william@spatialpixel.com>
2024-04-09 00:55:20 -0400

    Close dropdowns by clicking outside of them

--- src/dropdown-menu.js
+++ src/dropdown-menu.js
@@ -12,18 +12,27 @@ class DropdownMenu extends HTMLElement {
     this.shadowRoot.appendChild(templateContent.cloneNode(true));
   }
 
-  connectedCallback() {
+  connectedCallback () {
     const toggleButton = this.shadowRoot.querySelector('.dropdown-toggle');
     
-    toggleButton.addEventListener('click', () => {
+    toggleButton.addEventListener('click', event => {
+      event.stopPropagation();
+      
       this.closeAllDropdownsExceptThis();
 
       const dropdownContent = this.shadowRoot.querySelector('.dropdown-content');
       dropdownContent.classList.toggle('show');
     });
+    
+    const dropdownContent = this.shadowRoot.querySelector('.dropdown-content');
+    dropdownContent.addEventListener('click', event => {
+      event.stopPropagation();
+    });
+    
+    this.addEventListener('dropdown-close', this.closeDropdown.bind(this));
   }
 
-  closeAllDropdownsExceptThis() {
+  closeAllDropdownsExceptThis () {
     const allDropdowns = document.getElementsByTagName('dropdown-menu');
     for (const dropdown of allDropdowns) {
       if (dropdown !== this) {
@@ -32,7 +41,7 @@ class DropdownMenu extends HTMLElement {
     }
   }
 
-  closeDropdown() {
+  closeDropdown () {
     const dropdownContent = this.shadowRoot.querySelector('.dropdown-content');
     dropdownContent.classList.remove('show');
 
@@ -40,6 +49,19 @@ class DropdownMenu extends HTMLElement {
     this.dispatchEvent(new CustomEvent('dropdownClosed'));
   }
 }
+
+window.document.body.addEventListener('click', event => {
+  const closeEvent = new CustomEvent('dropdown-close', {
+    detail: {
+      origin: 'DropdownMenu'
+    }
+  });
+  
+  const dropdowns = window.document.querySelectorAll('dropdown-menu');
+  for (const dropdown of dropdowns) {
+    dropdown.dispatchEvent(closeEvent);
+  }
+});
 
 // Define the custom element
 customElements.define('dropdown-menu', DropdownMenu);
