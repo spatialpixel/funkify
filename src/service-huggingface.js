6bf9bc3c0accce95b5b182a413a2ba4b39eac94d
William Martin <william@spatialpixel.com>
2025-10-01 20:42:58 -0400

    update hugging face models list

--- src/service-huggingface.js
+++ src/service-huggingface.js
@@ -9,19 +9,19 @@ export default class HuggingFaceService extends LLMService {
     super(state, 'huggingface');
     // this.state = the app's state singleton
     // this.serviceKey = a string like 'openai' or 'huggingface'
-    
+
     this.keyManager = document.querySelector(`key-manager#${this.serviceKey}-api-key`);
     this.keyManager.initialize(this.getter.bind(this), this.setter.bind(this));
   }
-  
+
   initialize () {
     if (!this.instance || this.apiKeyChanged) {
       this.instance = new HfInference(this.apiKey);
-      
+
       this.apiKeyChanged = false;
     }
   }
-  
+
   preprocessParams (params) {
     const isImageMessage = message => {
       const gotAnArray = _.isArray(lastMessage.content);
@@ -29,63 +29,64 @@ export default class HuggingFaceService extends LLMService {
       const imageUrls = _.filter(lastMessage.content, msg => msg.type === "image_url");
       return _.size(imageUrls) > 0;
     };
-    
+
     const isToolMessage = message => message.role === 'tool';
-    
+
     const lastMessage = _.last(params.messages);
-    
+
     // TODO: Check that all tool calls were satisfied.
-    
+
     // There are two bugs in HuggingFace Inference API. Tool calls only resolve if there is no
     // 'tools' field, and image messages seem to trigger tools even if the inference doesn't work.
     if (isToolMessage(lastMessage) || isImageMessage(lastMessage)) {
       delete params["tools"]
     }
   }
-  
+
   async createTextCompletion (params) {
     this.preprocessParams(params);
-    
+
     console.debug(`huggingface completion params:`, params);
-    
+
     // return await this.instance.chatCompletionStream(params);
     return await this.instance.chatCompletion(params);
   }
-  
+
   get models () {
+    // HACK: For llama and qwen models, tool_calls should have content: "". So ignore "tools" once the call is done.
+    // Also vision models with images need to ignore "tools".
     return [
-      // tool_calls should have content: "", HACK: to ignore "tools" once the call is done.
-      'meta-llama/Meta-Llama-3-8B-Instruct',
-      
-      // tool_calls should have content: "", HACK: to ignore "tools" once the call is done.
+      'openai/gpt-oss-120b',
+      'Qwen/Qwen3-Coder-480B-A35B-Instruct',
+      'Qwen/Qwen3-Coder-30B-A3B-Instruct',
       'Qwen/Qwen2.5-72B-Instruct',
-      
-      // when images are present, need to ignore "tools"
+      'Qwen/Qwen2.5-Coder-32B-Instruct',
+      'meta-llama/Meta-Llama-3-8B-Instruct',
       'meta-llama/Llama-3.2-11B-Vision-Instruct',
-      
-      // Works similarly to the above models. Commented out as a manual-entry example.
-      // "NousResearch/Hermes-3-Llama-3.1-8B",
-    ]
+      'NousResearch/Hermes-3-Llama-3.1-8B',
+      'zai-org/GLM-4.5',
+      'zai-org/GLM-4.1V-9B-Thinking',
+    ];
   }
-  
+
   modelSupportsVision (model) {
     const modelsWithVision = ['meta-llama/Llama-3.2-11B-Vision-Instruct'];
     return modelsWithVision.includes(model);
   }
-  
+
   get stream () {
     return false;
   }
-  
+
   processToolCallsMessage (message) {
     if (!_.isObject(message)) { return }
-    
+
     if (_.isArray(message.tool_calls)) {
       // The tool_calls message needs a "content": "" field, which HF requires but omits.
       message.content = "";
-      
+
       _.forEach(message.tool_calls, tool_call => {
-        // tool_call messages from Hugging Face don't have robust IDs, but some models like 
+        // tool_call messages from Hugging Face don't have robust IDs, but some models like
         // those from Mistral, still require them. They don't work, but it's still worth putting
         // the logic here. Iterate over all the tool calls and ensure they have unique IDs.
         if (_.isEmpty(tool_call.id) || _.size(tool_call.id) !== 9) {
@@ -93,7 +94,7 @@ export default class HuggingFaceService extends LLMService {
           tool_call.id = uid.rnd();
         }
       });
-      
+
       // TODO: track tool IDs and ensure all tool calls have been satisfied.
     }
   }
